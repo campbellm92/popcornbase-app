9f37fc84d84afe3c85264fed210d455a61a8377c
Matthew Campbell <m.campbell92@gmail.com>
2024-07-19 15:59:15 +0200

    configure route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const fetchMovies = require("../handlers/movieHandler");
 
-router.get("/", (req, res) => {
-  res.status(200).render("index", {
-    message: "Hello there",
-  });
+router.get("/", async (req, res) => {
+  const minRating = req.query.minRating;
+  const maxRating = req.query.maxRating;
+
+  if (minRating && maxRating) {
+    try {
+      const movie = await fetchMovies(minRating, maxRating);
+      res.status(200).render("index", { movie });
+    } catch (err) {
+      console.error(err);
+      res.status(500).render("index", { error: "Could not fetch movie." });
+    }
+  } else {
+    res.status(200).render("index", { movie: null });
+  }
 });
 
 module.exports = router;
