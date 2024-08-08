8d33a58b966ecf093ae47786f2d853149b42273d
Matthew Campbell <m.campbell92@gmail.com>
2024-08-08 12:36:58 +0200

    attempt to create slide animation for form, not working

---
+++ public/js/slideForm.js
@@ -0,0 +1,10 @@
+// function slideForm() {
+//   //   event.preventDefault();
+//   const form = document.getElementById("slideForm");
+
+//   form.classList.add("form-slid-position");
+
+//   setTimeout(function () {
+//     form.submit();
+//   }, 5000);
+// }
