5036f2129dd4f78ab1b18efb7725db01b4996ed2
Matthew Campbell <m.campbell92@gmail.com>
2024-08-09 18:58:24 +0200

    remove uncessary code

--- public/js/getMoviesClient.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const actionBtn = document.getElementById("actionBtn");
-//   const form = document.querySelector("form");
-
-//   actionBtn.addEventListener("click", function () {
-//     form.submit();
-//   });
-// });
