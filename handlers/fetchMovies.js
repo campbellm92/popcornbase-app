55f7890a8b8954651842accb3dfad22a61c27fa5
Matthew Campbell <m.campbell92@gmail.com>
2024-08-09 16:21:47 +0200

    remove comments, add error handling

--- handlers/fetchMovies.js
+++ handlers/fetchMovies.js
@@ -3,14 +3,6 @@ const axios = require("axios");
 
 const TMDB_KEY = process.env.TMDB_KEY;
 
-// const options = {
-//   method: "GET",
-//   url: ,
-//   headers: {
-//     accept: "application/json",
-//   },
-// };
-
 async function fetchMovies(minRating, maxRating) {
   const page = Math.floor(Math.random() * 500) + 1;
   try {
@@ -18,8 +10,13 @@ async function fetchMovies(minRating, maxRating) {
       `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_KEY}&page=${page}&vote_average.gte=${minRating}&vote_average.lte=${maxRating}`
     );
     const data = res.data;
-    console.log(data);
     const movies = data.results;
+
+    if (movies.length === 0) {
+      console.log("No movies found");
+    }
+    console.log(movies);
+
     const randomMovie = movies[Math.floor(Math.random() * movies.length)];
     return randomMovie;
   } catch (err) {
