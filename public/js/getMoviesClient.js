e5d7ac4babff1bc5dc56e69e257cc7ced104a47c
Matthew Campbell <m.campbell92@gmail.com>
2024-08-09 16:22:35 +0200

    submit function pending redundency

---
+++ public/js/getMoviesClient.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const actionBtn = document.getElementById("actionBtn");
+//   const form = document.querySelector("form");
+
+//   actionBtn.addEventListener("click", function () {
+//     form.submit();
+//   });
+// });
