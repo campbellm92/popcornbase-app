0b6f1d5b23bc294cca6f17539e91c80fb95e5910
Matthew Campbell <m.campbell92@gmail.com>
2024-08-24 16:51:42 +0200

    separate pw hashing and verification logic

---
+++ utils/passwordUtils.js
@@ -0,0 +1,21 @@
+const argon2 = require("argon2");
+
+async function hashPassword(password) {
+  try {
+    const hash = await argon2.hash(password);
+    return hash;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function verifyPassword(hash, password) {
+  try {
+    return await argon2.verify(hash, password);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+module.exports = { hashPassword, verifyPassword };
