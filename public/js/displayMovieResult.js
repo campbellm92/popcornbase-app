bcdee36434a1de97f0add5763638e5ba3d7f204e
Matthew Campbell <m.campbell92@gmail.com>
2024-08-31 11:40:06 +0200

    add padding to poster

--- public/js/displayMovieResult.js
+++ public/js/displayMovieResult.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (data.movie) {
         html = `<h3 class="text-center rec-title mt-4">Your movie recommendation:</h3>
               <div class="card mx-auto mt-4 mb-5 d-flex flex-row" style="max-width: 540px;">
-                  <img src="https://image.tmdb.org/t/p/w200${data.movie.poster_path}" alt="Movie poster" class="img-fluid rounded-start">
+                  <img src="https://image.tmdb.org/t/p/w200${data.movie.poster_path}" alt="Movie poster" class="img-fluid rounded-start p-1">
                   <div class="card-body">
                   <h5 class="card-title">${data.movie.title}</h5>
                   <p class="card-text">${data.movie.overview}</p>
