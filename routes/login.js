7a0fe4d13cab82b3d5f3b433304ebe96bcc4a250
Matthew Campbell <m.campbell92@gmail.com>
2024-10-07 18:15:22 +0200

    add jwt logic, add check for email/password entered

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const {
-  getUserByEmail,
-  verifyUserPassword,
-} = require("../queries/userQueries");
+const { getUserByEmail } = require("../queries/userQueries");
+const { verifyPassword } = require("../utils/passwordUtils");
 
-// const JWT_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const JWT_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -16,20 +14,40 @@ router.get("/login", (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).render("login", {
+        requiredError:
+          "A valid email address and password are required to proceed.",
+      });
+    }
+
     const user = await getUserByEmail(email);
     if (!user) {
-      res.status(400).render("login", {
+      return res.status(400).render("login", {
         invalidUserError:
           "The email you've entered could not be found. Please try again.",
       });
     }
-    const correctPassword = await verifyUserPassword(password);
+
+    const correctPassword = await verifyPassword(user.password, password);
     if (!correctPassword) {
-      res.status(400).render("login", {
+      return res.status(400).render("login", {
         incorrectPasswordError:
           "The password you've entered is incorrect. Please try again.",
       });
     }
+
+    const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.cookie("authToken", token, {
+      httpOnly: true,
+      secure: true, //possibly change bc might not set with http during production
+      maxAge: 60 * 60 * 1000,
+    });
+
     res.status(200).render("login", {
       successfulLogin: "Login successful. You'll now be redirected.",
     });
