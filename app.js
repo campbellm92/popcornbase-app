f5fd198224dd0f1d972c564e1008973751daed08
Matthew Campbell <m.campbell92@gmail.com>
2024-10-08 14:43:15 +0200

    import verify jwt logic, logout page

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const verifyAuth = require("./middleware/verifyAuth.js");
 const indexRouter = require("./routes/index.js");
 const signupRouter = require("./routes/signup.js");
 const loginRouter = require("./routes/login.js");
+const watchlistRouter = require("./routes/watchlist.js");
+const logoutRouter = require("./routes/logout.js");
 
 const app = express();
 
@@ -15,15 +19,23 @@ app.set("view engine", "hbs");
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(verifyAuth);
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated;
+  next();
+});
 
 // Ensure that express can read static files
 app.use(express.static(path.join(__dirname, "public")));
 
 // Use the routers
 app.use("/", indexRouter);
-app.use("/", signupRouter);
-app.use("/", loginRouter);
+app.use("/signup", signupRouter);
+app.use("/login", loginRouter);
+app.use("/watchlist", watchlistRouter);
+app.use("/logout", logoutRouter);
 
 module.exports = app;
